5efd6c95e71b033d0896d255cb1a47e038022549
Jorge Raul Ochoa-Reyes <jorge@bytekitchen.io>
2024-07-15 10:48:27 -0700

    added metrics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { GeistSans } from "geist/font/sans";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-
+import { type AppType } from "next/app"; 
+import { Analytics } from "@vercel/analytics/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -15,6 +15,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <main data-theme="sunset" className={GeistSans.className}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </SessionProvider>
   );
