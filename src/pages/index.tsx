dc13851a89a13f600364261acf6689788fa5d807
Jorge Raul Ochoa-Reyes <jorge@bytekitchen.io>
2024-09-19 23:05:04 -0700

    minor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
     }
     return newText;
   };
-  const checkIfColorIsDark = (hex: string): boolean => {
+  const checkIfColorIsDark = (hex: string): string => {
     const r = parseInt(hex.substring(1, 3), 16);
     const g = parseInt(hex.substring(3, 5), 16);
     const b = parseInt(hex.substring(5, 7), 16); 
